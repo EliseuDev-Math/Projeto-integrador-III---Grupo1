dd564170dadfe35ab82e78ce8dd0a8252cc63692
EliseuDev-Math <eliseualexandre496@gmail.com>
2026-03-31 17:39:26 -0400

    Create BaseModel.java

---
+++ main/java/com/petgato/model/BaseModel.java
@@ -0,0 +1,39 @@
+package com.projeto.model;
+
+import java.time.LocalDateTime;
+
+public class BaseModel {
+
+    private Long id;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataAtualizacao;
+
+    public BaseModel() {
+        this.dataCriacao = LocalDateTime.now();
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
